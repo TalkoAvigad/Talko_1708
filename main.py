a1e0d86ce63eb53a2b46ae6b2d447950efaca927
Tal Avigad <talko.cytwist@gmail.com>
2022-10-24 18:04:02 +0300

    First commit from branch

--- main.py
+++ main.py
@@ -1,1 +1,2 @@
-print("Hi Python, I'm Talko and i create that project to check how to work with GitHub.")
+print("Hi Python, I'm Talko and i create that project to check how to work with GitHub.)
+print("Hi I'm open new branch")
